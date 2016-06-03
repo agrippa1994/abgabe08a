2a355093e0a3f50d1081045518c2a2e163ab5a82
agrippa1994 <agrippa1994@icloud.com>
2016-06-03 14:26:41 +0200

    Added comments

--- abgabe08a/src/at/fhj/itm/StringStack.java
+++ abgabe08a/src/at/fhj/itm/StringStack.java
@@ -12,16 +12,28 @@ public class StringStack implements Stack
 	private String[] data;
 	private int offset = -1;
 	
+	/**
+	 * Initializes Stack
+	 * @param size Number of elements
+	 */
 	public StringStack(int size) {
 		data = new String[size];
 	}
-	
+
+	/**
+	 * Checks if Stack is empty
+	 * @return true if empty otherwise false
+	 */
 	@Override
 	public boolean isEmpty() {
 		return offset <= -1;
 	}
 
 	@Override
+	/**
+	 * Pushes element onto the stack
+	 * @param item String element
+	 */
 	public void push(String item) {
 		if(offset + 1 >= data.length) {
 			System.out.println("Stack is full!");
@@ -31,6 +43,9 @@ public class StringStack implements Stack
 		data[++offset] = item;
 	}
 
+	/**
+	 * Pops element from stack
+	 */
 	@Override
 	public String pop() {
 		if(isEmpty())
